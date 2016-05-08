4fe1aa95f1dbe9c6fa9b0b13f59f8b27622fb778
Ayush Jha <Ayush Jha>
2016-05-09 00:30:32 +0530

    initial commit

---
+++ public/javascripts/app.js
@@ -0,0 +1,187 @@
+'use strict';
+
+/**********************************************************************
+ * Angular Application
+ **********************************************************************/
+var app = angular.module('app', ['ngResource', 'ngRoute'])
+  .config(function($routeProvider, $locationProvider, $httpProvider) {
+    //================================================
+    // Check if the user is connected
+    //================================================
+    var checkLoggedin = function($q, $timeout, $http, $location, $rootScope){
+      // Initialize a new promise
+      var deferred = $q.defer();
+
+      // Make an AJAX call to check if the user is logged in
+      $http.get('/loggedin').success(function(user){
+        // Authenticated
+        if (user !== '0'){
+          /*$timeout(deferred.resolve, 0);*/
+          window.localStorage.setItem('username', user.github.username);
+          deferred.resolve();
+        }
+        // Not Authenticated
+        else {
+          $rootScope.message = 'You need to log in.';
+          //$timeout(function(){deferred.reject();}, 0);
+          deferred.reject();
+          window.location.href='/auth/github';
+        }
+      });
+
+      return deferred.promise;
+    };
+    
+    function checkUser($http){
+      $http.get('/loggedin').success(function(user){
+      if (user !== '0'){
+        console.log('hello', user.github.username);
+        window.localStorage.setItem('username', user.github.username);
+      }
+      else{
+        console.log('nah');
+      }
+      });
+    }
+    //================================================
+    
+    //================================================
+    // Add an interceptor for AJAX errors
+    //================================================
+    $httpProvider.interceptors.push(function($q, $location) {
+      return {
+        response: function(response) {
+          // do something on success
+          return response;
+        },
+        responseError: function(response) {
+          if (response.status === 401)
+          //$location.url('/login');
+          window.location.href='/auth/github';
+          return $q.reject(response);
+        }
+      };
+    });
+    //================================================
+
+    //================================================
+    // Define all the routes
+    //================================================
+    $routeProvider
+      .when('/', {
+        templateUrl: '/views/main.html',
+        controller: 'YelpCtrl',
+        resolve: {
+          app: checkUser
+        }
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+    //================================================
+
+  }) // end of config()
+  .run(function($rootScope, $http){
+    $rootScope.message = '';
+
+    // Logout function is available in any pages
+    $rootScope.logout = function(){
+      localStorage.clear();
+      $rootScope.message = 'Logged out.';
+      $http.post('/logout');
+    };
+  });
+
+/**********************************************************************
+ * Yelp controller
+ **********************************************************************/
+app.controller('YelpCtrl', function($scope, $http, $location) {
+  var $search = $('#filter-search');
+  
+  $search.keypress(function(e) {
+    if (e.which == 13) {
+      var input = this.value;
+      search(input);
+      $('.contain').addClass('active');
+      $('.bars-list').removeClass('fadeInUp').addClass('fadeOut');
+      window.localStorage.setItem('prevSearch', input);
+    }
+  });
+  
+  if(window.localStorage.getItem('save') && window.localStorage.getItem('prevSearch')){
+    console.log('this should save the search');
+    $('.contain').addClass('active');
+    search(window.localStorage.getItem('prevSearch'));
+    window.localStorage.removeItem('save');
+  }
+  
+  $scope.image = function(bar){
+    console.log(bar.image_url);
+    if(!bar.image_url){
+      bar.image_url = 'http://cumbrianrun.co.uk/wp-content/uploads/2014/02/default-placeholder-300x300.png';
+    }
+  };
+  
+  $scope.attending = function(bar) {
+    var user = window.localStorage.getItem('username');
+    if(user && bar.userArray.indexOf(user)==-1){
+      var data;
+      console.log(bar.userArray);
+      if(bar.userArray.length <= 0){
+        console.log('POST IT');
+        bar.userArray.push(user);
+        data = {users: bar.userArray};
+        $http.post('/app/yelp/'+bar.id, data).success(function(response){
+          console.log('response: ', response);
+        });
+      }
+      else{
+        console.log('PUT IT');
+        bar.userArray.push(user);
+        data = {users: bar.userArray};
+        $http.put('/app/yelp/'+bar.id, data).success(function(){
+          console.log('PUT resquest success');
+        });
+      }
+      
+    }
+    else if(bar.userArray.indexOf(user) > -1)
+      console.log('you are already attending');
+    else{
+      console.log('you need to log in');
+      window.localStorage.setItem('save', true);
+      window.location.href='/auth/github';
+    }
+  };
+  
+  $scope.remove = function(bar){
+      var index = bar.userArray.indexOf(window.localStorage.getItem('username'));
+      bar.userArray.splice(index,1);
+      console.log(bar.userArray);
+      var data = {users: bar.userArray};
+      if (bar.userArray.length <= 0){
+        console.log('DELETED');
+        $http.delete('/app/yelp/'+bar.id);
+      }
+      else{
+        $http.put('/app/yelp/'+bar.id, data);
+      }
+  };
+  
+  $scope.alreadyGoing = function(bar){
+    var user = window.localStorage.getItem('username');
+    if(user && bar.userArray.indexOf(user)>-1)
+      return true;
+    else
+      return false;
+  };
+
+  function search(location) {
+    $http.get('/app/yelp/' + location).success(function(response) {
+      $scope.businesses = response;
+      //$('.bars-list').addClass('animated fadeInDown');
+      console.log(response);
+    });
+  }
+
+});
---
+++ server.js
@@ -0,0 +1,198 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var passport = require('passport');
+var GitHubStrategy = require('passport-github').Strategy;
+var mongoose = require('mongoose');
+
+//Yelp API setup
+var Yelp = require('yelp');
+var yelp = new Yelp({
+    consumer_key: 'DjfBzsjOO-2An8vwm2rt5w',
+    consumer_secret: 'MT5uJsUr4wRL0M9cSM8grHaPipo',
+    token: 'HExyIn5wsNfcfKK76y6qgQPKXU_Y8EIL',
+    token_secret: 'NHASOJ4wl2Wx6hBN8JsyXdphKRA'
+});
+
+require('dotenv').load();
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var YelpSchema = new Schema({
+	id: String,
+	users: Array
+});
+var UserSchema = new Schema({
+	github: {
+		id: String,
+		displayName: String,
+		username: String,
+    publicRepos: Number
+	},
+	attending: Array
+});
+
+mongoose.model('Business', YelpSchema);
+mongoose.model('User',UserSchema);
+
+var Bar = mongoose.model('Business');
+var User = mongoose.model('User');
+var configAuth={'githubAuth': {
+		'clientID': process.env.GITHUB_KEY,
+		'clientSecret': process.env.GITHUB_SECRET,
+		'callbackURL': process.env.APP_URL + 'auth/github/callback'
+}};
+
+	passport.serializeUser(function (user, done) {
+		done(null, user.id);
+	});
+
+	passport.deserializeUser(function (id, done) {
+		User.findById(id, function (err, user) {
+			done(err, user);
+		});
+	});
+
+	passport.use(new GitHubStrategy({
+		clientID: configAuth.githubAuth.clientID,
+		clientSecret: configAuth.githubAuth.clientSecret,
+		callbackURL: configAuth.githubAuth.callbackURL
+	},
+	function (token, refreshToken, profile, done) {
+		process.nextTick(function () {
+			User.findOne({'github.id': profile.id}, function (err, user) {
+				if (err) {
+					return done(err);
+				}
+
+				if (user) {
+					return done(null, user);
+				} else {
+					var newUser = new User();
+
+					newUser.github.id = profile.id;
+					newUser.github.username = profile.username;
+					newUser.github.displayName = profile.displayName;
+					newUser.github.publicRepos = profile._json.public_repos;
+					
+					newUser.save(function (err) {
+						if (err) {
+							throw err;
+						}
+
+						return done(null, newUser);
+					});
+				}
+			});
+		});
+	}));
+
+var auth = function(req, res, next){
+  if (!req.isAuthenticated()) 
+  	res.send(401);
+  else
+  	next();
+};
+
+var app = express();
+
+mongoose.connect(process.env.MONGODB_URI || process.env.MONGODB_URI);
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.cookieParser()); 
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.session({ secret: 'securedsession' }));
+app.use(passport.initialize()); // Add passport initialization
+app.use(passport.session());    // Add passport initialization
+app.use(app.router);
+app.use(express.static(__dirname + "/public"));
+
+//==================================================================
+// routes
+app.get('/users', auth, function(req, res){
+  //res.send([{name: "user1"}, {name: "user2"}]);
+});
+
+// yelp routes
+app.get('/app/yelp/:location', function(req,res){
+  yelp.search({term: 'nightlife', location: req.params.location}, function(err,data){
+    if(err) throw err;
+    (data.businesses).forEach(function(each){
+    	Bar.findOne({id : each.id}, function(err,obj){
+    		if(err)
+    			throw err;
+    		if(obj !== null){
+    			each.userArray = obj.users;	
+    		}
+    		else{
+    			each.userArray = [];
+    		}
+    	});
+    });
+    setTimeout(function(){res.json(data.businesses)},300);
+  });
+});
+
+app.post('/app/yelp/:bar',auth,function(req,res){
+	var attended = new Bar();
+	attended.id = req.params.bar;
+	attended.users = req.body.users;
+	attended.save(function(err) {
+        if (err) throw err;
+        res.json(attended);
+    });
+});
+
+app.put('/app/yelp/:bar', auth, function(req,res){
+	Bar.update({id:req.params.bar}, {$set: {users: req.body.users}},
+	function(err) {
+    if (err) throw err;
+  });
+});
+
+app.delete('/app/yelp/:bar', auth, function(req,res){
+	var bar = req.params.bar;
+    //console.log(id);
+    Bar.remove({
+        id: ''+bar+''
+    }, function(err) {
+        if (err) throw err;
+        res.status(204).end();
+    });
+});
+
+//==================================================================
+
+//==================================================================
+// route to test if the user is logged in or not
+app.get('/loggedin', function(req, res) {
+  res.send(req.isAuthenticated() ? req.user : '0');
+});
+
+// route to log in
+app.post('/login', passport.authenticate('github'), function(req, res) {
+  res.json(req.user);
+});
+
+// route to log out
+app.post('/logout', function(req, res){
+  req.logOut();
+  res.send(200);
+});
+
+app.get('/auth/github', passport.authenticate('github'));
+
+	app.get('/auth/github/callback', passport.authenticate('github', {
+			successRedirect: '/',
+			failureRedirect: '/login'
+		}));
+
+//==================================================================
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+    console.log('Listening on port ' + port + '...');
+});
